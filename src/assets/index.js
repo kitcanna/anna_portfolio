5623882fe9470195e3651ffc2c8d2b46ad9449fc
kitcanna <89905869+kitcanna@users.noreply.github.com>
2023-03-07 11:49:46 +0100

    portfolio v.1

---
+++ src/assets/index.js
@@ -0,0 +1,60 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import git from "./tech/git.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import python from "./tech/python.png";
+import java from "./tech/java.png";
+import sql from "./tech/sql.png";
+import cplus from "./tech/cplus.png";
+import dax from "./tech/dax.png";
+import matlab from "./tech/matlab.png";
+
+import cgi from "./company/cgi.png";
+import wharton from "./company/wharton.png";
+import google from "./company/google.png";
+import psm from "./company/psm.png";
+import tetrapak from "./company/tetrapak.png";
+import mau from "./company/mau.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  git,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  python, java, sql, cplus, dax, matlab,
+  cgi, wharton, google, psm, tetrapak, mau,
+  tailwind,
+  typescript,
+  threejs,
+  carrent,
+  jobit,
+  tripguide,
+
+
+};
---
+++ src/constants/index.js
@@ -0,0 +1,315 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    reactjs,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,   
+    python, java, sql, cplus, dax, matlab,
+    cgi, wharton, google, psm, tetrapak, mau,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "experience",
+      title: "Timeline",
+    },
+    {
+      id: "keyskills",
+      title: "Key Skills",
+    },
+    {
+      id: "projects",
+      title: "Projects",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Computer Science & Telecommunications Engineering Student",
+      icon: web,
+    },
+    {
+      title: "Business/Data Analyst",
+      icon: mobile,
+    },
+    {
+      title: "Project Manager",
+      icon: backend,
+    },
+    {
+      title: "Scrum Master",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "Python",
+      icon: python,
+    },
+    {
+      name: "Java",
+      icon: java,
+    },
+    {
+      name: "SQL",
+      icon: sql,
+    },
+    {
+      name: "C/C++",
+      icon: cplus,
+    },
+    {
+      name: "DAX",
+      icon: dax,
+    },
+    {
+      name: "MATLAB",
+      icon: matlab,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    }
+  ];
+
+  const experiences = [
+    {
+      title: "Participant in the Female Student Acceleration Program",
+      company_name: "CGI",
+      icon: cgi,
+      iconBg: "#383E56",
+      date: "Feb 2023 - Present",
+      points: [
+        "This program is a selective, exclusive membership network designed for top female students, providing opportunities for growth, inspiration and relationship building.",
+      ],
+    },
+    {
+      title: "Business Analytics Specialization",
+      company_name: "The Wharton School of the University of Pennsylvania",
+      icon: wharton,
+      iconBg: "#383E56",
+      date: "Feb 2023",
+      points: [
+        "A comprehensive 5 course program, culminating in a capstone project.",
+      ],
+    },
+    {
+      title: "Professional Scrum Master Certification (PSM I)",
+      company_name: "Scrum.org",
+      icon: psm,
+      iconBg: "#383E56",
+      date: "Jul 2022",
+      points: [
+        "",
+      ],
+    },
+    {
+      title: "Google Data Analytics Professional Certificate",
+      company_name: "Google via Coursera",
+      icon: google,
+      iconBg: "#383E56",
+      date: "Jul 2022",
+      points: [
+        "",
+      ],
+    },
+    {
+      title: "Student Talent Programme Consultant",
+      company_name: "Tetra Pak (via Adecco)",
+      icon: tetrapak,
+      iconBg: "#383E56",
+      date: "Apr 2022 - Present",
+      points: [
+        "I spearheaded a digitization, visualization, and automation project for production management worldwide.",
+        "My aim was to develop a tool to streamline workflows and provide valuable insights through facilitating data‑driven decision‑making and enhancing a cross‑functional team’s productivity and efficiency on a intercontinental scale."
+      ],
+    },
+    {
+      title: "Google Project Management Professional Certificate",
+      company_name: "Google via Coursera",
+      icon: google,
+      iconBg: "#383E56",
+      date: "Feb 2022",
+      points: [
+        "",
+      ],
+    },
+    {
+      title: "Computer Science & Telecommunications Engineering",
+      company_name: "Malmö University",
+      icon: mau,
+      iconBg: "#383E56",
+      date: "Aug 2021 - degree expected Jun 2024",
+      points: [
+        "CGPA: 3.92/4.00",
+        "Awarded two scholarships for 2021; one for good academic results and one for exceptional dedication at my faculty.",
+        "Student representative since 2021.",
+        "Project Leader for Malmö University Introduction Committee from 2021‑2022."
+      ],
+    },
+    {
+      title: "President/Chairman of the Board",
+      company_name: "BRF Borgmästaregården 1 i Lund (Housing cooperative/tenant‑owners’ society) ",
+      icon: "",
+      iconBg: "#E6DEDD",
+      date: "Apr 2019 - Present",
+      points: [
+        "Directed the society for 4 years by working collaboratively with board members to establish organizational objectives.",
+        "Through the implementation of multiple strategic plans with specific, measurable outcomes, I evaluated the progress of these outcomes to expand the organizational and economic capacity.",
+        "By utilizing these measures, I was also able to navigate challenges posed by the pandemic and ensure the continued success of the organization."
+      ],
+    },
+    {
+      title: "Restaurant‑, Café‑ and Event Manager",
+      company_name: "Stamstället, Café LUX, Moroten och Piskan",
+      icon: "",
+      iconBg: "#E6DEDD",
+      date: "Nov 2015 ‑ Jun 2021",
+      points: [
+        "Increased revenue and helped set 3 record sale years in a row by leading and training personnel and conducting day‑to‑day project coordination, planning, and implementation across multiple teams.",
+        "Service, fine dining, barista, bartender, garde‑manager, events and more."
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "Business Analytics Specialization",
+      name: "The Wharton School of the University of Pennsylvania",
+      designation: "Wharton",
+      company: "Badge:",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Project Leader & Manager for Malmö University Introduction Committee",
+      description:
+        "A year long extracurricular project ending in 5 weeks of grandiose activities and events for all of the university’s new students (approx. 2000 students), led by volunteering older students, with the purpose of helping them finding friends and their place within the university. I was the manager of all aspects of project‑, budget‑ and coordination management, and developer of all marketing and front‑end solutions.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
